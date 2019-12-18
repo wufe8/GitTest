3bb96ada0f03922d48ce6cfdaaa39a6f86f60b27
wufe8 <wufeo8@gmail.com>
2019-12-18 23:50:29 +0800

    first update

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class elephant
+{
+private:
+	flont size;
+	int age;
+	bool sex;
+public:
+	void getSize();
+	elephant(age,sex);
+	//TODO:interface
+}
+
+int main()
+{
+	cout << "hello world!";
+	return 0;
+
+}
